fe268a84560e8111c9dfcbcb32149dcd729620de
Emerson Soares <dev.emerson@gmail.com>
2012-06-15 01:25:20 -0400

    Cake core include path configured

--- webroot/index.php
+++ webroot/index.php
@@ -58,7 +58,7 @@ if (!defined('APP_DIR')) {
  *
  * Leaving this constant undefined will result in it being defined in Cake/bootstrap.php
  */
-//define('CAKE_CORE_INCLUDE_PATH',  DS . 'var' . DS . 'www' . DS . 'libs' . DS . 'cake' . DS . 'lib');
+define('CAKE_CORE_INCLUDE_PATH',  DS . 'var' . DS . 'www' . DS . 'libs' . DS . 'cake' . DS . 'lib');
 
 /**
  * Editing below this line should NOT be necessary.
