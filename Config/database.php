1c3886f201a92075e95a7fbaedeae56aa188d469
Emerson Soares <dev.emerson@gmail.com>
2012-06-15 09:43:28 -0400

    changed database driver to MySql

--- Config/database.php
+++ Config/database.php
@@ -3,7 +3,7 @@
 class DATABASE_CONFIG {
     
     public $default = array(
-        'datasource' => 'Database/Postgres',
+        'datasource' => 'Database/MySql',
         'persistent' => false,
         'host' => 'ec2-107-22-235-143.compute-1.amazonaws.com',
         'login' => 'avhgnnansr',
