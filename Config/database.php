0ed0d1f8cbd0c5aa7e930f6a87a79e4297eae030
Emerson Soares <dev.emerson@gmail.com>
2012-06-15 04:14:14 -0400

    database.php unignored

---
+++ Config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+class DATABASE_CONFIG {
+
+    public $default = array(
+        'datasource' => 'Database/Mysql',
+        'persistent' => false,
+        'host' => '',
+        'login' => '',
+        'password' => '',
+        'database' => '',
+        'prefix' => '',
+    );
+
+}
